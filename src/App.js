e62141d49fdfac2c253915c99edbb8201db67a4d
arturo <ahpareja@gmail.com>
2022-06-02 14:55:12 -0500

    Set up categories page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Nav from './components/nav'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+      </div>
+      <Routes>
+        
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <div className="header-container">
+        <h1>Bookstore</h1>
+        <nav>
+          <Link to="/">BOOKS</Link>
+          <Link to="/categories">CATEGORIES</Link>
+        </nav>
+      </div>
+  );
+}
+
+export default Nav
