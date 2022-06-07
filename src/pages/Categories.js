d170a3f8af00f4b89a4b0d050d1f4907fd0f5ae0
arturo <ahpareja@gmail.com>
2022-06-07 15:10:01 -0500

    add category message

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div className="categories">
-    <button type="button">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.category);
+  const showStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div className="categories">
+      <button type="button" onClick={showStatus}>Check Status</button>
+      <span>{status}</span>
+    </div>
+  );
+};
 
 export default Categories;
