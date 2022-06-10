798b6b81b9c76f913501748002ab8ac998cdadbb
arturo <ahpareja@gmail.com>
2022-06-09 21:32:44 -0500

    Add user icon

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,13 +5,14 @@ import './nav.css';
 function Nav() {
   return (
     <div className="header-container">
-      <h1>Bookstore</h1>
+      <h1 className="title">Bookstore CMS</h1>
       <nav className="nav">
         <Link to="/">BOOKS</Link>
         {' '}
         {' '}
         <Link to="/categories">CATEGORIES</Link>
       </nav>
+      <i className="fas fa-user" />
     </div>
   );
 }
